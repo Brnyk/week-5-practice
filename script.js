3b5095385f539b23a93290787005e82c662b74b1
Hornyák <bence.hornyak63@gmail.com>
2023-10-25 18:21:21 +0200

    object-checking

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
-/* let valtozoNeve = 10
-valtozoNeve = 20
-
-
-const valtozoNeve2 = 10
-
-// valtozoNeve2 = 20 // const-ot nem lehet így változtatni
-
-
-let array = [1, 2, 3,]
-array = [4, 5, 6]
-array.push(10)
-
-
-const array2 = [10, 20, 30]
-array2.push(200)
-array2[0] = 11 */
-
-
-const arr = ["nagy bence", "kiss józsi", "tóth jános"]
-
-for(let i = 0; i < arr.length;i++)
-console.log(arr[i])
-
-
-arr.forEach((value, index) => {
-    console.log("at index: ", index, " is: ",value)
-    console.log(`at index: ${index} is: ${value}`) // template literal, ${jsExpression} <- template literal expression
-})
-
-
-let mapResult = arr.map((value, index) => {
-    console.log(`at index: ${index} is: ${value}`)
-})
-
-
-
-for (const value of arr) {
-    console.log(`forof: ${value}`)
-    
+let obj = {
+    key1: "value",
+    key2: 100,
+    key3: true,
+    key4: ['one', 'two', 'three', 'four'],
+    key5: {
+        nestedKey1: "nested value",
+        nestedKey2: 200,
+        nestedKey3: false,
+        nestedKey4: ["ein", "zwei", "drei"],
+        nestedKey5: { nestedKey: "hello",}
+    }
 }
+
